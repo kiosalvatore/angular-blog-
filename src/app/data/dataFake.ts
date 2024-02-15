f114bbece0fe5ee7f8a1f9eb4a3907c92b25e80b
kiosalvatore <cariocacaiogomes@gmail.com>
2024-02-15 20:02:06 -0300

    add commit pages

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
     {
-        "id":1,
-        "photo":"aaa",
-        "title": "saiu novo filme",
-        "description": "ola"
+        "id":"1",
+        "title": "saiu novo filme do HOMEM DE FERRO",
+        "description": "FALA NERD, PETE JORDAN AQUI",
+        "photoCover": "https://media.contentapi.ea.com/content/dam/news/www-ea/images/2022/09/ea-motive-new-title-teaser-16x9-featured.jpg.adapt.crop16x9.575p.jpg"
     },
     {
-        "id":2,
-        "photo":"aaa",
-        "title": "saiu novo filme",
-        "description": "ola"
+        "id":"2",
+        "title": "nova série do HULK anunciada, HULK IMMORTAL vem aí?",
+        "description": "blblbllblblba",
+        "photoCover": "https://www.blogs.unicamp.br/ciencianerd/wp-content/uploads/sites/113/2021/11/20210514-immortal-hulk-alex-ross-cover-art-800x445.jpg"
     }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -6,13 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  photoCover:string = "https://t.ctcdn.com.br/jSEs-a2AsizaO2xZCQXcdbGPZW0=/i490793.jpeg"
-  contentTitle:string = "myNEWS"
-  contentDescription:string = "blalaalalalllallala"
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentDescription:string = ""
 
-  constructor() { }
+  private id:string | null = "0"
+
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id)
   }
 
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+  
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+  }
 }
